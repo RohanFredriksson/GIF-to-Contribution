39efded434fcebcccc2bc05fac77377f3fec543d
RohanFredriksson <rfre3327@uni.sydney.edu.au>
2022-10-23 00:53:51 +1100

    Improved theme validation and string formatting.

--- generate.py
+++ generate.py
@@ -1,25 +1,36 @@
 import numpy as np
-import sys
 import cv2
+import sys
 import os
 
 COLUMNS = 53
 PIXEL_SIZE = 11
 PIXEL_SPACING = 8
 
+THEME_STEPS = 5
 DEFAULT_THEME = "light"
 
 def validate_video(video: str):
 
     if not os.path.exists(video):
-        return False, "video '" + video + " could not be found"
+        return False, "video '{}' could not be found".format(video)
 
     return True, ""
 
 def validate_theme(theme: str):
 
     if not os.path.isdir("./themes/" + theme):
-        return False, "theme '" + theme + "' could not be found"
+        return False, "theme '{}' could not be found".format(theme)
+
+    for i in THEME_STEPS:
+
+        img_path = "./themes/{}/{}.png".format(theme, i)
+        if not os.path.exists(img_path):
+            return False , "image '{}.png' could not be found in theme '{}'".format(i, theme)
+
+        img = cv2.imread(img_path)
+        if img.shape[0] != PIXEL_SIZE or img.shape[1] != PIXEL_SIZE:
+            return False , "image '{}.png' has incorrect dimensions ({}, {}), image should have dimensions ({}, {})".format(img.shape[0], img.shape[1], PIXEL_SIZE, PIXEL_SIZE)
 
     return True, ""
 
@@ -30,7 +41,7 @@ def main():
 
     # Video argument is required.
     if len(sys.argv) < 2:
-        print(sys.argv[0] + ": missing file arguments")
+        print("{}: missing file arguments".format(sys.argv[0]))
         return
 
     # Store the video argument
@@ -39,7 +50,7 @@ def main():
     # Ensure that the video is valid.
     valid, reason = validate_video(video)
     if not valid:
-        print(sys.argv[0] + ": " + reason)
+        print("{}: {}".format(sys.argv[0], reason))
         return
 
     # See if the user inputted a theme.
@@ -49,7 +60,7 @@ def main():
     # Ensure that the theme is valid.
     valid, reason = validate_theme(theme)
     if not valid:
-        print(sys.argv[0] + ": " + reason)
+        print("{}: {}".format(sys.argv[0], reason))
         return
 
     width = (PIXEL_SIZE * COLUMNS) + (PIXEL_SPACING * (COLUMNS - 1))
