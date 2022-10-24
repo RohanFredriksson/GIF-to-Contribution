95aca87fea29d3dec2804e4782240a73edcd1922
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2022-10-25 00:05:06 +1100

    Improved argument parsing and documentation.

--- generate.py
+++ generate.py
@@ -25,6 +25,7 @@ class Theme:
 
         for i in range(THEME_STEPS):
 
+            # For each contribution box store both its image and grayscale value.
             img = cv2.imread("./themes/{}/{}.png".format(name, i))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(img)
@@ -32,10 +33,12 @@ class Theme:
             self.values.append(min(i * 64, 255))
 
     def get_closest_image(self, value: int):
-
+        
+        # If we have already computed it for this value, return it.
         if value in self.cache:
             return self.cache[value]
 
+        # Find the closest grayscale box value for this value.
         closest_distance = float('inf')
         closest_value = None
         closest_img = None
@@ -47,6 +50,7 @@ class Theme:
                 closest_value = self.values[i]
                 closest_img = self.images[i]
         
+        # Store it in the cache, and return the closest image.
         self.cache[closest_value] = closest_img
         return closest_img
 
@@ -154,73 +158,80 @@ class VideoIterator:
 
 def main():
 
+    # Generate the argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="A video file. Currently supported formats are .mp4 and .gif.")
+    parser.add_argument("-t", "--theme", help="The name of a theme located in the themes directory.", default=DEFAULT_THEME)
+    parser.add_argument("-o", "--output", help="The name of the file where the gif should be saved.", default=DEFAULT_OUTPUT)
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Set variables
     theme = None
     video = None
-
-    theme_name = DEFAULT_THEME
-    video_name = None
-
-    output = DEFAULT_OUTPUT
-
-    # Video argument is required.
-    if len(sys.argv) < 2:
-        print("{}: missing file arguments".format(sys.argv[0]))
-        return
-
-    # Store the video argument
-    video_name = sys.argv[1]
+    video_name = args.input
+    theme_name = args.theme
+    output = args.output
 
     # Ensure that the video is valid.
     valid, reason = Video.validate(video_name)
     if not valid:
         print("{}: {}".format(sys.argv[0], reason))
         return
-
-    # See if the user inputted a theme.
-    if len(sys.argv) > 2:
-        theme_name = sys.argv[2]
-
+        
     # Ensure that the theme is valid.
     valid, reason = Theme.validate(theme_name)
     if not valid:
         print("{}: {}".format(sys.argv[0], reason))
         return
 
+    # Theme name and video are valid, create the objects
     theme = Theme(theme_name)
     video = Video(video_name)
 
+    # Figure out how many rows are required for the image.
     rows = round(COLUMNS / video.aspect_ratio)
     contribution_width = (PIXEL_SIZE * COLUMNS) + (PIXEL_SPACING * (COLUMNS - 1))
     contribution_height = (PIXEL_SIZE * rows) + (PIXEL_SPACING * (rows - 1))
 
+    # Create lists to store image and duration data.
     frames = []
     durations = []
 
     a = 0
+    # Loop through every frame and duration in the video
     for frame, duration in video:
 
         print(a)
 
+        # Take the frame, downsize it to the correct size and convert it to grayscale.
         frame = cv2.resize(frame, (COLUMNS, rows), interpolation=cv2.INTER_AREA)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # Create the new frame to store the contribution gif frame.
         contribution_frame = Image.new(mode="RGBA", size=(contribution_width, contribution_height))
         for i in range(rows):
             for j in range(COLUMNS):
 
+                # For each pixel in the grayscale, match it to a git contribution box.
                 pixel = theme.get_closest_image(gray[i][j])
 
+                # Find the bounding box to paste the contribution box into.
                 top = i * (PIXEL_SIZE + PIXEL_SPACING)
                 bottom = top + PIXEL_SIZE
                 left = j * (PIXEL_SIZE + PIXEL_SPACING)
                 right = left + PIXEL_SIZE
 
+                # Paste the contribution box
                 contribution_frame.paste(pixel, box=(left, top, right, bottom))
 
+        # Add the frame and duration to the list.
         frames.append(contribution_frame)
         durations.append(duration)
         a += 1
 
+    # Save all frames as a gif.
     frames[0].save(output, save_all=True, append_images=frames[1:], duration=durations, loop=0, transparency=0)
 
 if __name__ == '__main__':
